96a752fa2c1c650b69f55a1fc42f3e6c404eaebc
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-08-08 12:14:53 +0100

    CSharp.Intro set up and shows the string when run dotnet

---
+++ CSharp.Intro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; // <- imports classes, such as `Console` that are defined in the `System` namespace
+
+namespace CSharp.Intro // <- a container (or scope) for classes
+{
+  internal class Program // <- this is a class
+  {
+    static void Main(string[] args) // <- static methods are like Ruby's class methods
+    // void means that this method returns nothing
+    // the main method takes an array of strings as an arg - `string[]`
+    {
+      Console.WriteLine("Hello Makers!"); // <- the main method is called automatically when you execute the program
+    }
+  }
+}
