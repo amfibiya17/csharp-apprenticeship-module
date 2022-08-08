a02976dbf02af5b543f07da67c14bdf206dd028b
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-08-08 23:57:38 +0100

    TDD for Calculator

--- main/exercises/csharp_tdd_intro/Calculator.Tests/CalculatorTests.cs
+++ main/exercises/csharp_tdd_intro/Calculator.Tests/CalculatorTests.cs
@@ -3,35 +3,45 @@ namespace Calculator.Tests
   public class CalculatorTests
   {
 
-    [Test]
-    public void Setup_Test() {
-      Assert.Pass();
+    [Test] // [Test] <- a special tag, called an attribute which indicates that what follows is a test and that it should be executed as part of the test suite
+    public void Setup_Test() { // <- Setup_Test is a regular C# method, which will be executed as a test
+      Assert.Pass(); // <- Assert is a class from the nunit framework
     }
 
-    // [Test]
-    // [TestCase(1, 1)]
-    // [TestCase(1, 2)]
-    // [TestCase(1, 3)]
-    // [TestCase(3, 1)]
-    // public void Add_TwoNumbers_ReturnTotal(int first, int second)
-    // {
-    //   Calculator calculator = new Calculator();
-    //   int actual = calculator.Add(first, second);
-    //   int expected = first + second;
-    //   Assert.AreEqual(actual, expected);
-    // }
+    [Test]
+    [TestCase(1, 1)]
+    [TestCase(1, 2)]
+    [TestCase(1, 3)]
+    [TestCase(3, 1)]
+    public void Add_TwoNumbers_ReturnTotal(int first, int second)
+    {
+      // Arrange
+      Calculator calculator = new Calculator();
 
-    // [Test]
-    // [TestCase(1, 2)]
-    // [TestCase(2, 2)]
-    // [TestCase(3, 2)]
-    // [TestCase(4, 10)]
-    // public void Multiply_TwoNumbers_ReturnProduct(int first, int second)
-    // {
-    //   Calculator calculator = new Calculator();
-    //   int actual = calculator.Multiply(first, second);
-    //   int expected = first * second;
-    //   Assert.AreEqual(actual, expected);
-    // }
+      // Act
+      int actual = calculator.Add(first, second);
+      int expected = first + second;
+
+      // Assert
+      Assert.AreEqual(actual, expected);
+    }
+
+    [Test]
+    [TestCase(1, 2)]
+    [TestCase(2, 2)]
+    [TestCase(3, 2)]
+    [TestCase(4, 10)]
+    public void Multiply_TwoNumbers_ReturnProduct(int first, int second)
+    {
+      // Arrange
+      Calculator calculator = new Calculator();
+
+      // Act
+      int actual = calculator.Multiply(first, second);
+      int expected = first * second;
+
+      // Assert
+      Assert.AreEqual(actual, expected);
+    }
   }
 }
--- main/exercises/csharp_tdd_intro/Calculator/Program.cs
+++ main/exercises/csharp_tdd_intro/Calculator/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      new Calculator().Add(2, 1);
+      new Calculator().Multiply(3, 4);
+    }
+  }
+
+  class Calculator
+  {
+    public void Add(int first, int second)
+    {
+      int result = first + second;
+    }
+
+    public void Multiply(int first, int second)
+    {
+      int result = first * second;
+    }
+  }
+}
