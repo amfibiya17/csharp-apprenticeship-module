b870da55ac1ce20db103468493dae67199970bc0
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-08-11 10:16:49 +0100

    TDD for It_Has_GetItem_Method_Which_Returns_aListOfItems

--- ShoppingList/MyList.Tests/MyListTests.cs
+++ ShoppingList/MyList.Tests/MyListTests.cs
@@ -2,32 +2,48 @@ namespace MyList.Tests;
 
 public class MyListTests
 {
-    [Test]
-    public void Initializes_Empty_List_Of_Item_Names()
-    {
-      // Arrange
-      MyList myList = new MyList();
+  [Test]
+  public void Initializes_Empty_List_Of_Item_Names()
+  {
+    // Arrange
+    MyList myList = new MyList();
 
-      // Assert
-      Assert.AreEqual(myList.list, new List<string>());
-    }
+    // Assert
+    Assert.AreEqual(myList.list, new List<string>());
+  }
 
-    [Test]
-    [TestCase("Item1")]
-    [TestCase("Item2")]
-    public void It_Has_AddItem_Method_WhichAdds_a_String_To_aListOfItems(string item)
-    {
-      // Arrange
-      MyList myList = new MyList();
+  [Test]
+  [TestCase("Item1")]
+  [TestCase("Item2")]
+  public void It_Has_AddItem_Method_WhichAdds_a_String_To_aListOfItems(string item)
+  {
+    // Arrange
+    MyList myList = new MyList();
 
-      // Act
-      myList.AddItem(item);
-      List<string> actual = myList.list;
-      List<string> expected = new List<string>() { item };
+    // Act
+    myList.AddItem(item);
+    List<string> actual = myList.list;
+    List<string> expected = new List<string>() { item };
 
-      // Assert
-      Assert.AreEqual(actual, expected);
-    }
+    // Assert
+    Assert.AreEqual(actual, expected);
+  }
 
-    
+  [Test]
+  [TestCase("Item1")]
+  [TestCase("Item2")]
+  public void It_Has_GetItem_Method_Which_Returns_aListOfItems(string item)
+  {
+    // Arrange
+    MyList myList = new MyList();
+
+    // Act
+    myList.AddItem(item);
+    List<string> actual = myList.list;
+    List<string> expected = new List<string>() { item };
+    myList.GetItems();
+
+    // Assert
+    Assert.AreEqual(actual, expected);
+  }
 }
--- ShoppingList/MyList/MyList.cs
+++ ShoppingList/MyList/MyList.cs
@@ -14,6 +14,11 @@ namespace MyList
     public void AddItem(string item)
     {
       list.Add(item);
-    } 
+    }
+
+    public List<string> GetItems()
+    {
+      return list;
+    }
   }
 }
