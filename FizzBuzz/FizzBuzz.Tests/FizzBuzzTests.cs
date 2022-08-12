343c02a37f446359372f51c6f0b4accb96e7a44e
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-08-12 11:00:20 +0100

    TDD for Numbers Not Divisible By Three Or Five

--- FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -1,15 +1,23 @@
 namespace FizzBuzz.Tests;
 
-public class Tests
+public class FizzBuzzTests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
+  [Test]
+  [TestCase(1, 1)]
+  [TestCase(4, 4)]
+  [TestCase(7, 7)]
+  [TestCase(11, 11)]
+  [TestCase(13, 13)]
+  public void ForNumbersNotDivisibleByThreeOrFive_ReturnTheNumber(int input, int output)
+  {
+    // Arrange
+    FizzBuzz fizzBuzz = new FizzBuzz();
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
+    // Act
+    int actual = fizzBuzz.For(input);
+    int expected = output;
+
+    // Assert
+    Assert.AreEqual(actual, expected);
+  }
 }
---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FizzBuzz 
+{
+  public class FizzBuzz
+  {
+    public int For(int number)
+    {
+      return number;
+    }
+  }
+}
